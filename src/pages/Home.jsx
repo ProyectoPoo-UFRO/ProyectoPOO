3263c986a144495786ace176cbd430c3ff27ffe3
Javier-Cortes228 <j.cortes10@ufromail.cl>
2025-11-30 23:59:52 -0300

    RESET

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import VendingMachine from "../components/VendingMachine/VendingMachine";
-
-export default function Home() {
-    return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-100 p-4">
-            <VendingMachine />
-        </div>
-    );
-}
